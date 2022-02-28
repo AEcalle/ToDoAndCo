ac1709c083ce6cd67221ab6ac02bccfa39753aa9
Aurélien Ecalle <contact@aurelienecalle.fr>
2022-02-28 10:40:14 +0100

    Cache With Redis

--- src/UseCase/User/List/ListHandler.php
+++ src/UseCase/User/List/ListHandler.php
@@ -6,22 +6,29 @@ namespace App\UseCase\User\List;
 
 use App\Repository\UserRepository;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
 #[AsMessageHandler]
 final class ListHandler
 {
-    public function __construct(private UserRepository $userRepository)
+    public function __construct(private UserRepository $userRepository, private TagAwareCacheInterface $redisCache)
     {
         $this->userRepository = $userRepository;
+        $this->redisCache = $redisCache;
     }
 
     public function __invoke(ListQuery $query): mixed
     {
-        return $this->userRepository->findBy(
-            $query->getCriteria(),
-            $query->getOrderBy(),
-            $query->getOffset(),
-            $query->getLimit()
-        );
+        $list = $this->redisCache->get('userLists', function(ItemInterface $item) use ($query) {
+            $item->tag(['Users', 'offset'.$query->getOffset(), 'limit'.$query->getLimit()]);
+            return $this->userRepository->findBy(
+                $query->getCriteria(),
+                $query->getOrderBy(),
+                $query->getOffset(),
+                $query->getLimit()
+            );
+        });
+        return $list;
     }
 }
--- tests/Unit/UseCase/Task/List/ListHandlerTest.php
+++ tests/Unit/UseCase/Task/List/ListHandlerTest.php
@@ -8,6 +8,7 @@ use App\Repository\TaskRepository;
 use App\UseCase\Task\List\ListHandler;
 use App\UseCase\Task\List\ListQuery;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
 class ListHandlerTest extends KernelTestCase
 {
@@ -16,8 +17,9 @@ class ListHandlerTest extends KernelTestCase
         self::bootKernel();
 
         $taskRepository = static::getContainer()->get(TaskRepository::class);
+        $redisCache = static::getContainer()->get(TagAwareCacheInterface::class);
         $nbTasks = $taskRepository->count([]);
-        $listHandler = new ListHandler($taskRepository);
+        $listHandler = new ListHandler($taskRepository, $redisCache);
 
         $tasks = $listHandler->__invoke(new ListQuery());
 
